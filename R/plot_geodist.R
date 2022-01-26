63e5e24e660842944c35ef39c6b6f57ec0709a72
Ludwigm6 <marvin.ludwig93@gmail.com>
2022-01-26 11:22:18 +0100

    document and test feature dist

--- R/plot_geodist.R
+++ R/plot_geodist.R
@@ -2,19 +2,25 @@
 #'
 #' @description Density plot of geographic nearest neighbor distances between training data as well as between training data and prediction locations.
 #' Optional, the nearest neighbor distances between training data and test data or between training data and CV iterations is shown.
-#' The plot can be used to check the suitability of a chosen CV method to be representative to estimate map accuracy.
+#' The plot can be used to check the suitability of a chosen CV method to be representative to estimate map accuracy. Alternatively distances can also be calculated in the multivariate feature space.
 #'
-#' @param x object of class sf or TrainDI
-#' @param modeldomain sf or raster object defining the prediction area
+#' @param x object of class sf, training data locations
+#' @param modeldomain sf or raster object defining the prediction area (see Details)
 #' @param cvfolds optional. List of row indices of x that are held back in each CV iteration. See e.g. ?createFolds or ?createSpaceTimeFolds
 #' @param testdata optional. object of class sf: Data used for independent validation
-#' @param samplesize numeric. How many random prediction samples should be used?
+#' @param samplesize numeric. How many random prediction samples should be used? Only required if modeldomain is a raster (see Details)
 #' @param scale logical. Present distances on log scale?
-#' @return A plot and a data.frame containing the spatial distances
+#' @param distance "geo" or "feature". Should the distance be computed in geographic space or in the multivariate predictor space (see Details)
+#' @return A plot and a data.frame containing the spatial/feature distances
+#'
+#'
+#'
+#' @details The modeldomain is a set of spatial points (sf points) that represent a sample of the prediction locations. If a raster is provided as modeldomain, the function takes a regular point sample (amount defined by samplesize) from the raster extent.
+#'     If distance = "feature", the arguments x, modeldomain and testdata have to include predictors. In this case, if you provide a raster as the model domain, the regular sample also  extracts the values from the raster layers.
 #'
 #' @import ggplot2
 #'
-#' @author Hanna Meyer, Edzer Pebesma
+#' @author Hanna Meyer, Edzer Pebesma, Marvin Ludwig
 #' @examples
 #' \dontrun{
 #' library(sf)
@@ -22,55 +28,59 @@
 #' library(caret)
 #' ########### prepare sample data:
 #' dat <- get(load(system.file("extdata","Cookfarm.RData",package="CAST")))
-#' dat <- aggregate(dat[,c("VW","Easting","Northing")],by=list(as.character(dat$SOURCEID)),mean)
-#' pts <- st_as_sf(dat,coords=c("Easting","Northing"))
+#' dat <- aggregate(dat[,c("DEM","TWI", "NDRE.M", "NDRE.Sd", "Bt", "Easting", "Northing")],by=list(as#' .character(dat$SOURCEID)),mean)
+#' pts <- dat[,-1]
+#' pts <- st_as_sf(pts,coords=c("Easting","Northing"))
 #' st_crs(pts) <- 26911
 #' pts_train <- pts[1:29,]
 #' pts_test <- pts[30:42,]
-#'
-#' studyArea <- raster(system.file("extdata","predictors_2012-03-25.grd",package="CAST"))
-#'
-#'
+#' studyArea <- raster::stack(system.file("extdata","predictors_2012-03-25.grd",package="CAST"))
+#' studyArea = studyArea[[c("DEM","TWI", "NDRE.M", "NDRE.Sd", "Bt")]]
 #' ########### Distance between training data and new data:
 #' dist <- plot_geodist(pts_train,studyArea)
-#'
 #' ########### Distance between training data, new data and test data:
 #' mapview(pts_train,col.regions="blue")+mapview(pts_test,col.regions="red")
 #' dist <- plot_geodist(pts_train,studyArea,testdata=pts_test)
-#'
 #' ########### Distance between training data, new data and CV folds:
 #' folds <- createFolds(1:nrow(pts_train),k=3,returnTrain=FALSE)
-#' plot_geodist(x=pts,modeldomain=studyArea,cvfolds=folds)
-#' }
+#' plot_geodist(x=pts_train, modeldomain=studyArea, cvfolds=folds)
 #'
+#' # with feature space distance
+#' plot_geodist(x=pts_train, modeldomain=studyArea, distance = "feature")
+#' plot_geodist(x=pts_train, modeldomain=studyArea, cvfolds = folds, testdata = pts_test,distance = "feature")
+#' }
 #'
 #'
 #'
 #'
 #' @export
 
-plot_geodist <- function(x,modeldomain,samplesize=2000,
-                         cvfolds=NULL,testdata=NULL,scale=TRUE,distance = "geo"){
-  x <- sf::st_transform(x,4326)
+plot_geodist <- function(x,
+                         modeldomain,
+                         samplesize=2000,
+                         cvfolds=NULL,
+                         testdata=NULL,
+                         scale=TRUE,
+                         distance = "geo"){
 
-  ##### Distance within training data:
 
+  # input formatting ------------
+  x = sf::st_transform(x,4326) # not sure if thus is needed
+
+
+  if(inherits(modeldomain, "Raster")){
+    modeldomain = sampleRaster(modeldomain, samplesize, distance)
+  }
+
+  # required steps ----
+  # always do sample-to-sample and sample-to-prediction
   s2s = sample2sample(x, distance)
-
-
-  # here if raster
-  #####
-
   s2p = sample2prediction(x, modeldomain, distance, samplesize)
 
-
-
-
-  ##### Compile data:
   dists = rbind(s2s, s2p)
 
+  # optional steps ----
   ##### Distance to test data:
-
   if(!is.null(testdata)){
     s2t = sample2test(x, testdata, distance)
     dists = rbind(dists, s2t)
@@ -82,7 +92,7 @@ plot_geodist <- function(x,modeldomain,samplesize=2000,
     dists = rbind(dists, cvd)
   }
 
-  ##### Compile and plot data:
+  # Compile and plot data ----
 
   p <- ggplot2::ggplot(data=dists, aes(x=dist, group=what, fill=what)) +
     ggplot2::geom_density(adjust=1.5, alpha=.4) +
@@ -223,8 +233,6 @@ sample2test = function(x, testdata, distance){
 
 # between folds
 
-
-
 cvdistance = function(x, cvfolds, distance){
 
   if(distance == "geo"){
@@ -263,32 +271,36 @@ cvdistance = function(x, cvfolds, distance){
 
 
 
-sampleRaster = function(modeldomain, samplesize){
+sampleRaster = function(modeldomain, samplesize, distance){
 
 
   ##### Distance to prediction locations:
   # regularly spread points (prediction locations):
   # see https://edzer.github.io/OGH21/
 
-  if (inherits(modeldomain, "Raster")){
-    if(samplesize>raster::ncell(modeldomain)){
-      samplesize <- raster::ncell(modeldomain)
-      message(paste0("samplesize for new data shouldn't be larger than number of pixels.
+  if(samplesize>raster::ncell(modeldomain)){
+    samplesize <- raster::ncell(modeldomain)
+    message(paste0("samplesize for new data shouldn't be larger than number of pixels.
               Samplesize was reduced to ",raster::ncell(modeldomain)))
-    }
-    modeldomainextent <- sf::st_as_sf(sf::st_as_sfc(sf::st_bbox(modeldomain)))
+  }
+  modeldomainextent <- sf::st_as_sf(sf::st_as_sfc(sf::st_bbox(modeldomain)))
 
-    sf::sf_use_s2(FALSE)
-    sf::st_as_sf(modeldomainextent) |>
-      sf::st_transform(4326) -> bb
-    methods::as(bb, "Spatial") |>
-      sp::spsample(n =samplesize, type = "regular")  |>
-      sf::st_as_sfc() |>
-      sf::st_set_crs(4326) -> predictionloc
-  }else if(inherits(modeldomain, "sf")){
-    predictionloc = modeldomain
+  sf::sf_use_s2(FALSE)
+  sf::st_as_sf(modeldomainextent) |>
+    sf::st_transform(4326) -> bb
+  methods::as(bb, "Spatial") |>
+    sp::spsample(n =samplesize, type = "regular")  |>
+    sf::st_as_sfc() |>
+    sf::st_set_crs(4326) -> predictionloc
+
+  predictionloc = st_as_sf(predictionloc)
+
+  if(distance == "feature"){
+    predictionloc = st_as_sf(raster::extract(modeldomain, predictionloc, df = TRUE, sp = TRUE))
+    predictionloc = na.omit(predictionloc)
   }
 
+  return(predictionloc)
 
 }
 
