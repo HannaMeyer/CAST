d87c222b7d7ac01919aaeb2dc9c733f9594fffa6
Chris Reudenbach <reudenbach@uni-marburg.de>
2023-01-02 21:48:13 +0100

    fix check on any first number of the band names

--- R/aoa.R
+++ R/aoa.R
@@ -147,7 +147,7 @@ aoa <- function(newdata,
   # handling of different raster formats
   as_stars <- FALSE
   as_terra <- FALSE
-  leading_digit <- all(grepl("[[:digit:]]",names(newdata)))
+  leading_digit <- any(grepl("^{1}[0-9]",n))
 
   if (inherits(newdata, "stars")) {
     if (!requireNamespace("stars", quietly = TRUE))
