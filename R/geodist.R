5fc1d8b876b5039871cc07afdddc8c9f39ae9c4b
HannaMeyer <hanna.meyer@uni-muenster.de>
2023-09-14 10:34:51 +0200

    code documentation

--- R/geodist.R
+++ R/geodist.R
@@ -180,9 +180,9 @@ geodist <- function(x,
   attr(dists, "type") <- type
 
   ##### Compute W if type=="geo" and test data and/or CV folds are provided
-  if(type == "geo"){
+  if(type == "geo"){ # take this condition out once tested for feature space as well
     W_sample <- twosamples::wass_stat(dists[dists$what == "sample-to-sample", "dist"],
-                                    dists[dists$what == "prediction-to-sample", "dist"])
+                                      dists[dists$what == "prediction-to-sample", "dist"])
     attr(dists, "W_sample") <- W_sample
     if(!is.null(testdata)){
       W_test <- twosamples::wass_stat(dists[dists$what == "test-to-sample", "dist"],
