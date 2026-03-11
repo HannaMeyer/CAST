dbd4e63be3bc62fa87e1bdeb294b7130f6bb76f2
Darius A. Görgen <info@dariusgoergen.com>
2026-03-11 14:38:14 +0100

    fix: reverses argument order in .mindistfun in aoa.R

--- R/aoa.R
+++ R/aoa.R
@@ -336,7 +336,7 @@ aoa <- function(newdata,
     }
     mindist <- rep(NA, nrow(newdata))
     mindist[okrows] <-
-      .mindistfun(newdataCC, train_scaled, k=1, method=method, algorithm=algorithm, S_inv=S_inv)
+      .mindistfun(train_scaled, newdataCC, k=1, method=method, algorithm=algorithm, S_inv=S_inv)
     DI_out <- mindist / trainDI$trainDist_avrgmean
   }
 
