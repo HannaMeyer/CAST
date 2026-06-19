139849ddbb48287ac21ffd651312eca88f8f1a88
HannaMeyer <hanna.meyer@uni-muenster.de>
2026-06-19 14:53:46 +0200

    formatting issues

--- tests/testthat/test-errorProfiles.R
+++ tests/testthat/test-errorProfiles.R
@@ -25,10 +25,10 @@ test_that("errorProfiles works in default settings", {
   expect_equal(round(as.numeric(summary(errormodel_DI$fitted.values)),2),
                c(14.25, 14.34, 15.21, 17.23, 18.70, 27.46))
   # test model predictions
-  expect_equal(as.vector(summary(terra::values(expected_error_DI))),
-               c("Min.   :14.26  ", "1st Qu.:27.46  ", "Median :27.46  ",
-                 "Mean   :26.81  ", "3rd Qu.:27.46  ","Max.   :27.47  ",
-                 "NA's   :17678  "))
+
+  s <- as.vector(summary(terra::values(expected_error_DI)))
+  s <- as.numeric(sub(".*:\\s*", "", s))
+  expect_equal(s,c(14.26,27.46,27.46,26.81,27.46,27.47,17678.00))
 })
 
 
@@ -54,10 +54,10 @@ test_that("errorProfiles works in with LPD", {
   expect_equal(round(as.numeric(summary(errormodel_LPD$fitted.values)),2),
                c(16.36, 16.36, 16.36, 16.36, 16.36, 16.36))
   # test model predictions
-  expect_equal(as.vector(summary(terra::values(expected_error_LPD))),
-               c("Min.   :16.36  ", "1st Qu.:16.36  ", "Median :16.36  ",
-                 "Mean   :16.36  ", "3rd Qu.:16.36  ",
-                 "Max.   :16.36  ", "NA's   :17678  "))
+
+  s <- as.vector(summary(terra::values(expected_error_LPD)))
+  s <- as.numeric(sub(".*:\\s*", "", s))
+  expect_equal(s,c(16.36,16.36,16.36,16.36,16.36,16.36,17678.00))
 
 })
 
@@ -85,12 +85,10 @@ test_that("errorProfiles works for multiCV", {
   expect_equal(round(as.numeric(summary(errormodel_DI$fitted.values)),2),
                c(12.53, 17.21, 26.80, 26.19, 35.28, 35.30))
   # test model predictions
-  expect_equal(as.vector( summary(terra::values(expected_error_DI))),
-               c("Min.   :13.11  ", "1st Qu.:32.58  ", "Median :35.05  ",
-                 "Mean   :32.54  ", "3rd Qu.:35.30  ",
-                 "Max.   :35.30  ", "NA's   :17678  "))
 
-
+  s <- as.vector(summary(terra::values(expected_error_DI)))
+  s <- as.numeric(sub(".*:\\s*", "", s))
+  expect_equal(s,c(13.11,32.58,35.05,32.54,35.30,35.30,17678.00))
 })
 
 test_that("plot for errorModel runs and returns ggplot", {
